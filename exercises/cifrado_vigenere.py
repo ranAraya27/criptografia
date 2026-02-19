fd31c4a5769cc1ccaf4e6dee0d9c5b0bc1b05bdc
Randall Eduardo Araya Nuñez <ranan2704@gmail.com>
2026-02-19 12:11:20 -0600

    feat() manejar mayusculas y minisculas en cifrado_vigenere

--- exercises/cifrado_vigenere.py
+++ exercises/cifrado_vigenere.py
@@ -1,18 +1,21 @@
 def funcion_generadora(texto, clave):
     clave_generada = ""
+    clave = clave.lower()
     for i in range(len(texto)):
         clave_generada += clave[i % len(clave)]
     return clave_generada
 
 
 def cifrado_vigenere(texto, clave):
-
     clave = funcion_generadora(texto, clave)
     resultado = ""
     base = 97
-    texto = texto.lower()
 
     for i in range(len(texto)):
+        if texto[i].isupper():
+            base = 65
+        else:
+            base = 97
         if texto[i].isalpha():
             letra_nueva = (ord(texto[i]) - base + (ord(clave[i]) - base)) % 26 + base
             resultado += chr(letra_nueva)
@@ -23,13 +26,15 @@ def cifrado_vigenere(texto, clave):
 
 
 def descifrado_vigenere(texto, clave):
-
     clave = funcion_generadora(texto, clave)
     resultado = ""
     base = 97
-    texto = texto.lower()
 
     for i in range(len(texto)):
+        if texto[i].isupper():
+            base = 65
+        else:
+            base = 97
         if texto[i].isalpha():
             letra_nueva = (ord(texto[i]) - base - (ord(clave[i]) - base)) % 26 + base
             resultado += chr(letra_nueva)
@@ -39,8 +44,8 @@ def descifrado_vigenere(texto, clave):
     return resultado
 
 
-palabra = "abcdef"
-clave = "bcd"
+palabra = "Hola que tal Lola"
+clave = "pez"
 cifrada = cifrado_vigenere(palabra, clave)
 print(cifrada)
 print(descifrado_vigenere(cifrada, clave))
