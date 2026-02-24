f82eed70f1f4258652d54a0f05cb3d611efac288
Randall Eduardo Araya Nuñez <ranan2704@gmail.com>
2026-02-24 15:56:52 -0600

    feat() modularización de código

--- exercises/cifrado_playfair.py
+++ exercises/cifrado_playfair.py
@@ -1,8 +1,4 @@
-def cifrado_playfair(mensaje, clave):
-    # Paso 1:
-    mensaje = mensaje.upper().replace("J", "I")
-    clave = clave.upper().replace("J", "I")
-
+def cifrado_playfair_paso1(clave):
     alfabeto = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
 
     matriz = [
@@ -22,7 +18,10 @@ def cifrado_playfair(mensaje, clave):
             matriz[i][j] = letras_matriz[k]
             k += 1
 
-    # Paso 2
+    return matriz
+
+
+def cifrado_playfair_paso2(mensaje):
     pares = []
     limpio = "".join([c for c in mensaje if c.isalpha()])
     i = 0
@@ -37,109 +36,76 @@ def cifrado_playfair(mensaje, clave):
             pares.append((a, b))
             i += 2
 
-    def pos(letra):
-        for i in range(5):
-            for j in range(5):
-                if matriz[i][j] == letra:
-                    return i, j
+    return pares
 
-    # Cifrado
-    cifrado = ""
+
+def pos(letra, matriz):
+    for i in range(5):
+        for j in range(5):
+            if matriz[i][j] == letra:
+                return i, j
+
+
+def playfair_forma_general(pares, matriz, modo="cifrar"):
+    texto = ""
+    operador = 1 if modo == "cifrar" else -1
     for par in pares:
         a = par[0]
         b = par[1]
 
-        fa, ca = pos(a)
-        fb, cb = pos(b)
+        fa, ca = pos(a, matriz)
+        fb, cb = pos(b, matriz)
 
         # Caso 1 distinta fila y columna
         if fa != fb and ca != cb:
-            cifrado += matriz[fa][cb]
-            cifrado += matriz[fb][ca]
+            texto += matriz[fa][cb]
+            texto += matriz[fb][ca]
         # Caso 2 misma fila
         elif fa == fb:
-            cifrado += matriz[fa][(ca + 1) % 5]
-            cifrado += matriz[fb][(cb + 1) % 5]
+            texto += matriz[fa][(ca + operador) % 5]
+            texto += matriz[fb][(cb + operador) % 5]
         # Caso 3 misma columna
         elif ca == cb:
-            cifrado += matriz[(fa + 1) % 5][ca]
-            cifrado += matriz[(fb + 1) % 5][cb]
+            texto += matriz[(fa + operador) % 5][ca]
+            texto += matriz[(fb + operador) % 5][cb]
 
-    print(cifrado)
-    return cifrado
+    return texto
+
+
+def cifrado_playfair(mensaje, clave):
+    mensaje = mensaje.upper().replace("J", "I")
+    clave = clave.upper().replace("J", "I")
+
+    # Paso 1: Crear matriz
+    matriz = cifrado_playfair_paso1(clave)
+
+    # Paso 2: Separar en pares
+    pares = cifrado_playfair_paso2(mensaje)
+
+    # Paso 3: Cifrar
+    return playfair_forma_general(pares, matriz, "cifrar")
 
 
 def descifrado_playfair(mensaje, clave):
-    # Paso 1:
     mensaje = mensaje.upper().replace("J", "I")
     clave = clave.upper().replace("J", "I")
 
-    alfabeto = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
+    # Paso 1: Crear matriz
+    matriz = cifrado_playfair_paso1(clave)
 
-    matriz = [
-        ["", "", "", "", ""],
-        ["", "", "", "", ""],
-        ["", "", "", "", ""],
-        ["", "", "", "", ""],
-        ["", "", "", "", ""],
-    ]
-    letras_matriz = ""
-    for c in clave + alfabeto:
-        if c not in letras_matriz:
-            letras_matriz += c
-    k = 0
-    for i in range(5):
-        for j in range(5):
-            matriz[i][j] = letras_matriz[k]
-            k += 1
+    # Paso 2: Separar en pares
+    pares = cifrado_playfair_paso2(mensaje)
 
-    # Paso 2
-    pares = []
-    limpio = "".join([c for c in mensaje if c.isalpha()])
-    i = 0
-    while i < len(limpio):
-        a = limpio[i]
-        b = limpio[i + 1] if i + 1 < len(limpio) else "X"
-
-        if a == b:
-            pares.append((a, "X"))
-            i += 1
-        else:
-            pares.append((a, b))
-            i += 2
-
-    def pos(letra):
-        for i in range(5):
-            for j in range(5):
-                if matriz[i][j] == letra:
-                    return i, j
-
-    # Cifrado
-    descifrado = ""
-    for par in pares:
-        a = par[0]
-        b = par[1]
-
-        fa, ca = pos(a)
-        fb, cb = pos(b)
-
-        # Caso 1 distinta fila y columna
-        if fa != fb and ca != cb:
-            descifrado += matriz[fa][cb]
-            descifrado += matriz[fb][ca]
-        # Caso 2 misma fila
-        elif fa == fb:
-            descifrado += matriz[fa][(ca - 1) % 5]
-            cifrado += matriz[fb][(cb - 1) % 5]
-        # Caso 3 misma columna
-        elif ca == cb:
-            descifrado += matriz[(fa - 1) % 5][ca]
-            descifrado += matriz[(fb - 1) % 5][cb]
-
-    print(descifrado.replace("X", ""))
-    return descifrado.replace("X", "")
+    # Paso 3: Descifrar
+    return playfair_forma_general(pares, matriz, "descifrar").replace("X", "")
 
 
 clave = "silla"
-cifrado = cifrado_playfair("hello world", clave)
-descifrado_playfair(cifrado, clave)
+palabra = "hello world"
+
+palabra_cifrada = cifrado_playfair(palabra, clave)
+palabra_descifrada = descifrado_playfair(palabra_cifrada, clave)
+
+print(palabra)
+print(palabra_cifrada)
+print(palabra_descifrada)
