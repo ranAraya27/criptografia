4813ea1cd577dc91db6dd41453785b2ee5cec3d8
Randall Eduardo Araya Nuñez <ranan2704@gmail.com>
2026-02-26 12:46:35 -0600

    cifrado hill primera version

---
+++ exercises/cifrado_hill.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+# Definir matrices
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[5, 6], [7, 8]])
+
+
+# Calcular la inversa
+A_inv = np.linalg.inv(A)
+
+
+val_let = {
+    "A": 0,
+    "B": 1,
+    "C": 2,
+    "D": 3,
+    "E": 4,
+    "F": 5,
+    "G": 6,
+    "H": 7,
+    "I": 8,
+    "J": 9,
+    "K": 10,
+    "L": 11,
+    "M": 12,
+    "N": 13,
+    "O": 14,
+    "P": 15,
+    "Q": 16,
+    "R": 17,
+    "S": 18,
+    "T": 19,
+    "U": 20,
+    "V": 21,
+    "W": 22,
+    "X": 23,
+    "Y": 24,
+    "Z": 25,
+}
+
+# Ejemplo de uso:
+# print(valores_letras['Z']) # Salida: 25
+
+
+def cifrado_hill(mensaje="act", clave="gybnqkurp"):
+    C = np.zeros((3, 3))
+    M = np.zeros((3, 1))
+    clave = clave.upper()
+    mensaje = mensaje.upper()
+    count = 0
+    for i in range(3):
+        for j in range(3):
+            C[i][j] = val_let[clave[count]]
+            count = count + 1
+
+    count = 0
+    for i in range(3):
+        M[i] = val_let[mensaje[count]]
+        count = count + 1
+
+    A = C @ M
+    for i in range(3):
+        A[i] = A[i] % 26
+    return A
+
+
+def cifrado_hill(matriz, clave="gybnqkurp"):
+    C = np.zeros((3, 3))
+    M = np.zeros((3, 1))
+    clave = clave.upper()
+    mensaje = mensaje.upper()
+    count = 0
+    for i in range(3):
+        for j in range(3):
+            C[i][j] = val_let[clave[count]]
+            count = count + 1
+
+    C_inv = np.linalg.inv(C)
+
+    A = C @ M
+    for i in range(3):
+        A[i] = A[i] % 26
+    return A
